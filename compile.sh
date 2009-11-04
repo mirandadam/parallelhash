f16d90d06f370f4c61903946a6ac7fe3d0ca64b8
Daniel Araújo Miranda <mirandadam@users.noreply.github.com>
2009-11-04 02:15:04 +0000

    Moved all code to folder src and modified compile script.

--- compile.sh
+++ compile.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-#gprof:
-#gcc -pg -g2 -c -O0 -Wall -Ilib lib/*.c
-#g++ -pg -g2 -c -O0 -Wall -fopenmp -I. -Ilib *.cpp
-#g++ -pg -g2 -O0 -Wall -fopenmp -o parallelhash *.o
+GCC=gcc-4.4
+GXX=g++-4.4
 
-#gcov:
-#gcc -c -fprofile-arcs -ftest-coverage -g2 -Wall -Ilib lib/*.c
-#g++ -c -fprofile-arcs -ftest-coverage -g2 -Wall -fopenmp -I. -Ilib *.cpp
-#g++ -fprofile-arcs -ftest-coverage -g2 -Wall -fopenmp -o parallel_hash *.o
+#degug:
+#$GCC -c -O0 -g2 -Wall -Isrc/lib src/lib/*.c
+#$GXX -c -O0 -g2 -Wall -fopenmp -Isrc -Isrc/lib src/*.cpp
+#$GXX -O0 -g2 -Wall -fopenmp -o parallelhash *.o
 
 #standard:
-gcc -c -O2 -Wall -Ilib lib/*.c
-g++ -c -O2 -Wall -fopenmp -I. -Ilib *.cpp
-g++ -O2 -Wall -fopenmp -o parallelhash *.o
+$GCC -c -O2 -Wall -Isrc/lib src/lib/*.c
+$GXX -c -O2 -Wall -fopenmp -Isrc -Isrc/lib src/*.cpp
+$GXX -O2 -Wall -fopenmp -o parallelhash *.o
 
 rm *.o
